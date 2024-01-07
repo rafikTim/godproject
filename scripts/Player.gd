34a2a6da2994e5c98b60eeb0955658fa7ca511c2
Snaker228 <77610623+Snaker228@users.noreply.github.com>
2024-01-07 15:50:39 +0300

    Еще пытаюсь сделать движение

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -8,4 +8,6 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if(delta >= 10):
+		apply_impulse(Vector3.FORWARD, Vector3(10, 0, 0))
 	pass
